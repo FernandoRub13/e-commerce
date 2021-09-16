a2201025b8e8ddc668f93be93459b6989d93e643
Fernando Rubio <pablo.rubio.bailon@gmail.com>
2021-09-16 10:34:13 -0500

    Maquetacion prinicpal del Homepage, Header y Directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import "./default.scss";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <h2>Helllo wolrd</h2>
+        <Header></Header>
+      <div className="main">
+        <Homepage></Homepage>
+      </div>
     </div>
   );
 }
